999996aed317bec51ca1bf8489f95af4695efddb
FelipeArruda15 <souzafelipesouza5@gmail.com>
2022-06-25 17:56:38 -0300

    Criação das classes modelos para Usuário e Role

---
+++ security/src/main/java/com/felipearruda/security/domain/Usuario.java
@@ -0,0 +1,25 @@
+package com.felipearruda.security.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Collection;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String nome;
+    private String username;
+    private String senha;
+    @ManyToMany(fetch = FetchType.EAGER)
+    private Collection<Role> roles = new ArrayList<>();
+}
