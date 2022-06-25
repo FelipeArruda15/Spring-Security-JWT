884853951b05029eb1c415bb1f02c9198f36ed3f
FelipeArruda15 <souzafelipesouza5@gmail.com>
2022-06-25 17:33:14 -0300

    Projeto criado

---
+++ security/src/test/java/com/felipearruda/security/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felipearruda.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
