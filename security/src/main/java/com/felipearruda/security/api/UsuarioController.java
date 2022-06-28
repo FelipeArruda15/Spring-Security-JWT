951cd28b0083ece4298d6d3e318e40a70c6f01ef
FelipeArruda15 <souzafelipesouza5@gmail.com>
2022-06-27 22:35:05 -0300

    Criando métodos para o controller de usuários

--- security/src/main/java/com/felipearruda/security/api/UsuarioController.java
+++ security/src/main/java/com/felipearruda/security/api/UsuarioController.java
@@ -1,13 +1,14 @@
 package com.felipearruda.security.api;
 
+import com.felipearruda.security.domain.Role;
 import com.felipearruda.security.domain.Usuario;
 import com.felipearruda.security.service.UsuarioService;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
+import javax.persistence.PostRemove;
 import java.util.List;
 
 @RestController
@@ -21,4 +22,27 @@ public class UsuarioController {
     public List<Usuario> buscarUsuarios(){
         return usuarioService.listarUsuarios();
     }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Usuario salvarUsuario(@RequestBody Usuario usuario){
+        return usuarioService.salvarUsuario(usuario);
+    }
+
+    @PostMapping("/role/salvar")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Role salvarRole(@RequestBody Role role){
+        return usuarioService.salvarRole(role);
+    }
+
+    @PostMapping("/role/addAoUsuario")
+    public void addRoleaoUsuario(@RequestBody FormFuncaodoUsuario form){
+        usuarioService.addRoleParaUsuario(form.getUsername(), form.getPerfil());
+    }
+}
+
+@Data
+class FormFuncaodoUsuario{
+    private String username;
+    private String perfil;
 }
