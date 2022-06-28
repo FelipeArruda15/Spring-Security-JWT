4ea765a3116d8a4268f74851a850396f8b54de02
FelipeArruda15 <souzafelipesouza5@gmail.com>
2022-06-27 22:52:12 -0300

    Populando banco de dados

--- security/src/main/java/com/felipearruda/security/SecurityApplication.java
+++ security/src/main/java/com/felipearruda/security/SecurityApplication.java
@@ -1,13 +1,43 @@
 package com.felipearruda.security;
 
+import com.felipearruda.security.domain.Role;
+import com.felipearruda.security.domain.Usuario;
+import com.felipearruda.security.service.UsuarioService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.ArrayList;
 
 @SpringBootApplication
 public class SecurityApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SecurityApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner executar(UsuarioService usuarioService){
+		return args -> {
+			usuarioService.salvarRole(new Role(null, "ROLE_USER"));
+			usuarioService.salvarRole(new Role(null, "ROLE_MANAGER"));
+			usuarioService.salvarRole(new Role(null, "ROLE_ADMIN"));
+			usuarioService.salvarRole(new Role(null, "ROLE_SUPER_ADMIN"));
+
+			usuarioService.salvarUsuario(new Usuario(null, "John Travolta", "john", "1234", new ArrayList<>()));
+			usuarioService.salvarUsuario(new Usuario(null, "Will Smith", "will", "1234", new ArrayList<>()));
+			usuarioService.salvarUsuario(new Usuario(null, "Jim Carrey", "jim", "1234", new ArrayList<>()));
+			usuarioService.salvarUsuario(new Usuario(null, "Adam Sandler", "adam", "1234", new ArrayList<>()));
+
+			usuarioService.addRoleParaUsuario("john", "ROLE_USER");
+			usuarioService.addRoleParaUsuario("john", "ROLE_MANAGER");
+			usuarioService.addRoleParaUsuario("will", "ROLE_MANAGER");
+			usuarioService.addRoleParaUsuario("jim", "ROLE_ADMIN");
+			usuarioService.addRoleParaUsuario("adam", "ROLE_SUPER_ADMIN");
+			usuarioService.addRoleParaUsuario("adam", "ROLE_ADMIN");
+			usuarioService.addRoleParaUsuario("adam", "ROLE_USER");
+		};
 	}
 
 }
